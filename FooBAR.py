729439761aae6214779e7c1dbebd2a2707f95d4a
NelsonBlakeN <nelson.blaken@gmail.com>
2018-03-07 11:31:59 -0600

    Small changes to main file

--- FooBAR.py
+++ FooBAR.py
@@ -4,6 +4,8 @@ DEV_PORT = ''   # Fill in by testing
 
 arduinoSerialData = serial.Serial(DEV_PORT, 9600)
 
+print("Running FooBAR...")
+
 while True:
     if arduinoSerialData.inWaiting() > 0:
         print(arduinoSerialData.readline())
