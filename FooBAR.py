cbfb85ee2d67566f19dcf08bb8dd8ac97a4e9aa6
NelsonBlakeN <nelson.blaken@gmail.com>
2018-03-08 11:17:30 -0600

    Defined Arduino port to listen on.

--- FooBAR.py
+++ FooBAR.py
@@ -1,6 +1,8 @@
 import serial   # Import serial library
+import time
+# from api.PythonAPI import PythonAPI
 
-DEV_PORT = ''   # Fill in by testing
+DEV_PORT = '/dev/ttyACM0'   # Fill in by testing
 
 arduinoSerialData = serial.Serial(DEV_PORT, 9600)
 
@@ -9,3 +11,4 @@ print("Running FooBAR...")
 while True:
     if arduinoSerialData.inWaiting() > 0:
         print(arduinoSerialData.readline())
+#	time.sleep(.5)
