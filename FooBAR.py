030e3efe04b45f19887666d351c5380506770278
NelsonBlakeN <nelson.blaken@gmail.com>
2018-03-27 11:13:47 -0500

    Successful finite state machine progression. Counter incrememnt/api call executed appropriately

--- FooBAR.py
+++ FooBAR.py
@@ -1,67 +1,42 @@
-import serial   # Import serial library
+import serial
 from api.PythonAPI import PythonAPI
 from TrafficCounterFSM import TrafficCounterFSM
 
-# Define constants: representing ports on the board that
-# are needed for the appropriate objects.
+# Define constants
 DEV_PORT = '/dev/ttyACM0'   # Serial port used by Arduino on Raspberry Pi
-FRONT_PIN_L = 0		        # Pin used by front left sensor
-BACK_PIN_L = 0		        # Pin used by back left sensor
-FRONT_PIN_R = 0		        # Pin used by front right sensor
-BACK_PIN_R = 0		        # Pin used by back right sensor
 
 # Creating necessary objects
-arduinoSerialData = serial.Serial(DEV_PORT, 9600)	    # Collect serial data from Ardunio
-left_fsm = TrafficCounterFSM(FRONT_PIN_L, BACK_PIN_L)	# Tracks state of left sensor set
-right_fsm = TrafficCounterFSM(FRONT_PIN_R, BACK_PIN_R)	# Tracks state of right sensor set
-api = PythonAPI()					                    # Python API object
+arduinoSerialData = serial.Serial(DEV_PORT, 9600)   # Collect serial data from Ardunio
+left_fsm = TrafficCounterFSM()	                    # Tracks state of left sensor set
+right_fsm = TrafficCounterFSM()	                    # Tracks state of right sensor set
+api = PythonAPI()					                # Python API object
 
 print("Running FooBAR...")
-
-# Tracks which sensor the current data belongs to
-iter = 0
 
 # Flush buffer before beginning
 arduinoSerialData.flushInput()
 
-# Synchronize input signals
-# x = arduinoSerialData.readline()
-# while x != "X":
-#     print(x)
-#     x = arduinoSerialData.readline()
-
 # Read data from Arduino
 while True:
     if arduinoSerialData.inWaiting() > 0:
-        # Recieved pin number of updated sensor
+        # Recieved byte containing sensor data
         data = arduinoSerialData.read()
-        data = (data == "1") # TODO: This will result in an "X" becoming 0. Possible
-                             # solution: send all data from Arduino in one byte
-        print(str(iter), ": Raw data=", str(data), "(", type(data), ")")
 
-	    # Tracks whether a person passed the box on either side
-        passed_left = 0
-        passed_right = 0
+        if data:
+            data = ord(data)
+            front_left_data  = data&1   # Front left sensor  = 1st bit
+            back_left_data   = data&2   # Back left sensor   = 2nd bit
+            front_right_data = data&4   # Front right sensor = 3rd bit
+            back_right_data  = data&8   # Back right sensor  = 4th bit
 
-        if iter == 0:
-            frontLeftData = data
-        elif iter == 1:
-            backLeftData = data
-        elif iter == 2:
-            frontRightData = data
-        elif iter == 3:
-            backRightData = data
-        elif iter == 4:
-            # Deliminator is reached, update FSMs
-            passed_left = left_fsm.updateFSM(frontLeftData, backLeftData)
-            passed_right = right_fsm.updateFSM(frontRightData, backRightData)
+            # Tracks whether a person passed the box on either side
+            passed_left = left_fsm.updateFSM(front_left_data, back_left_data)
+            passed_right = right_fsm.updateFSM(front_right_data, back_right_data)
 
-        # If a person passed, update the database through the API
-        if passed_left or passed_right:
-            api.person_passed()
-
-	    # Update iterator
-        if iter < 4:
-	        iter += 1
-        elif iter >= 4:
-	        iter = 0
+            # If a person passed, update the database once per side
+            if passed_left:
+                api.person_passed()
+            if passed_right:
+                api.person_passed()
+        else:
+            print("Data was none")
